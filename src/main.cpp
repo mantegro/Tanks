de77d7bed2ebe457fe02d4c1d7e34f5598ff467a
Alexander Shevchenko <alexander.shevchenko@3ds.com>
2017-02-15 16:09:46 +0100

    add all libraries to cmake, created a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,43 @@
-#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/vec3.hpp>
 
 int main(void)
 {
-    std::cout << "Hello world" << std::endl;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glm::vec3 color(0.3f, 0.7f, 1.f);
+    glClearColor(color.r, color.g, color.b, 1.f);
     
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
